0551a21841ed9451c085cd7ae9c5d7dabefed3bd
binhao.chen <binhao.chen@tcl.com>
2016-08-20 19:49:29 +0800

    food details

--- src/com/tct/restaurant/activity/FoodInfoActivity.java
+++ src/com/tct/restaurant/activity/FoodInfoActivity.java
@@ -6,14 +6,19 @@ import java.util.List;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.method.MovementMethod;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
@@ -24,17 +29,24 @@ import com.tct.restaurant.util.InjectView;
 import com.tct.restaurant.util.Injector;
 import com.tct.restaurant.util.RequestUtils;
 
-public class FoodInfoActivity extends Activity {
+public class FoodInfoActivity extends Activity implements OnClickListener{
     View v1;
     View v2;
-    TextView mCommentNum;
+    private TextView foodType;
     private TextView foodName;
     private TextView foodPrice;
     private TextView foodScore;
-    private TextView foodEvaluation;
     private TextView foodIntroduction;
+    private TextView foodIngredient;
+    private TextView foodInfo;
+    private TextView foodEvaluation;
+    private TextView foodPepperyNo;
+    private TextView foodPepperyLittle;
+    private TextView foodPepperyMiddle;
+    private TextView foodPepperyMost;
     private ImageView foodPicView;
     private Button foodAddBt;
+    private RatingBar rb;
     private String mFid = "";
     private FoodEntity mFoodEntity;
     public  List<FoodEntity> foodList_Current = new ArrayList<FoodEntity>();
@@ -49,30 +61,7 @@ public class FoodInfoActivity extends Activity {
 
         mFid = getIntent().getStringExtra("fid");
         Log.i("hao", "" + mFid);
-
-        foodName = (TextView) findViewById(R.id.food_name);
-        foodPrice = (TextView) findViewById(R.id.food_price);
-        foodScore = (TextView) findViewById(R.id.food_score);
-        foodEvaluation = (TextView) findViewById(R.id.food_comment_num);
-        foodIntroduction = (TextView) findViewById(R.id.food_info);
-        foodPicView = (ImageView) findViewById(R.id.food_pic);
-        foodEvaluation.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                
-            }
-        });
-        foodAddBt = (Button) findViewById(R.id.food_add_to_cart);
-        foodAddBt.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Log.i("hao", "HomeFragment click add to cart bt.. ");
-            }
-        });
+        
         init();
     }
 
@@ -85,6 +74,31 @@ public class FoodInfoActivity extends Activity {
         .build();                                   // 创建配置过得DisplayImageOption对象
 
     private void init() {
+        foodType = (TextView) findViewById(R.id.detail_food_type);
+        foodPicView = (ImageView) findViewById(R.id.food_pic);
+        foodName = (TextView) findViewById(R.id.food_name);
+        foodPrice = (TextView) findViewById(R.id.food_price);
+        rb = (RatingBar) findViewById(R.id.ratingBar);
+        foodScore = (TextView) findViewById(R.id.food_score);
+        foodIntroduction = (TextView) findViewById(R.id.food_introduction);
+        foodIntroduction.setOnClickListener(this);
+        foodIngredient = (TextView) findViewById(R.id.food_ingredient);
+        foodIngredient.setOnClickListener(this);
+        foodInfo = (TextView) findViewById(R.id.food_info);
+        foodPepperyNo = (TextView) findViewById(R.id.food_taste_peppery_no);
+        foodPepperyLittle = (TextView) findViewById(R.id.food_taste_peppery_little);
+        foodPepperyMiddle = (TextView) findViewById(R.id.food_taste_peppery_middle);
+        foodPepperyMost = (TextView) findViewById(R.id.food_taste_peppery_most);
+        foodPepperyNo.setOnClickListener(this);
+        foodPepperyLittle.setOnClickListener(this);
+        foodPepperyMiddle.setOnClickListener(this);
+        foodPepperyMost.setOnClickListener(this);
+        foodEvaluation = (TextView) findViewById(R.id.food_comment_num);
+        foodEvaluation.setOnClickListener(this);
+        foodAddBt = (Button) findViewById(R.id.food_add_to_cart);
+        foodAddBt.setOnClickListener(this);
+        
+        
         // TODO Auto-generated method stub
         foodList_Current = RequestUtils.foodList_Current;
         for (int i = 0; i < foodList_Current.size(); i++) {
@@ -93,12 +107,92 @@ public class FoodInfoActivity extends Activity {
             }
         }
         if (mFoodEntity != null) {
+            foodType.setText(mFoodEntity.getCategory());
             foodName.setText(mFoodEntity.getName());
-            foodPrice.setText(mFoodEntity.getPrice()+"元");
-            foodScore.setText("评分:"+mFoodEntity.getStars());
-            foodEvaluation.setText("评论 "+mFoodEntity.getEvaluation());
-            foodIntroduction.setText(mFoodEntity.getIntroduction());
+            foodPrice.setText("¥"+mFoodEntity.getPrice());
+            foodScore.setText(mFoodEntity.getStars()+"分");
+            rb.setRating(Float.parseFloat(mFoodEntity.getStars()));
+            foodEvaluation.setText(" "+mFoodEntity.getEvaluation()+"条");
+            foodInfo.setText(mFoodEntity.getIntroduction());
+            foodInfo.setMovementMethod(ScrollingMovementMethod.getInstance());
             ImageLoader.getInstance().displayImage(mFoodEntity.getImage(), foodPicView, options/*, null*/);
+        }
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+        case R.id.food_add_to_cart:
+            Log.i("hao", "HomeFragment click add to cart bt.. ");
+            //ying
+            break;
+        case R.id.food_introduction:
+            Log.i("hao", "HomeFragment onClick food_introduction");
+            foodIntroduction.setTextColor(getResources().getColor(R.color.tct_black));
+            foodIntroduction.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+            foodIngredient.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodIngredient.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+            foodInfo.setText(mFoodEntity.getIntroduction());
+            break;
+        case R.id.food_ingredient:
+            Log.i("hao", "HomeFragment onClick food_ingredient");
+            foodIngredient.setTextColor(getResources().getColor(R.color.tct_black));
+            foodIngredient.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+            foodIntroduction.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodIntroduction.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+            foodInfo.setText(mFoodEntity.getIngredient());
+            break;
+        case R.id.food_taste_peppery_no:
+            Log.i("hao", "HomeFragment onClick food_taste_peppery_no");
+            foodPepperyNo.setBackground(getResources().getDrawable(R.drawable.text_field_selected_black));
+            foodPepperyLittle.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyMiddle.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyMost.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyNo.setTextColor(getResources().getColor(R.color.tct_black));
+            foodPepperyLittle.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodPepperyMiddle.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodPepperyMost.setTextColor(getResources().getColor(R.color.tct_gray));
+            break;
+        case R.id.food_taste_peppery_little:
+            Log.i("hao", "HomeFragment onClick food_taste_peppery_little");
+            foodPepperyNo.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyLittle.setBackground(getResources().getDrawable(R.drawable.text_field_selected_black));
+            foodPepperyMiddle.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyMost.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyNo.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodPepperyLittle.setTextColor(getResources().getColor(R.color.tct_black));
+            foodPepperyMiddle.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodPepperyMost.setTextColor(getResources().getColor(R.color.tct_gray));
+            break;
+        case R.id.food_taste_peppery_middle:
+            Log.i("hao", "HomeFragment onClick food_taste_peppery_middle");
+            foodPepperyNo.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyLittle.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyMiddle.setBackground(getResources().getDrawable(R.drawable.text_field_selected_black));
+            foodPepperyMost.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyNo.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodPepperyLittle.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodPepperyMiddle.setTextColor(getResources().getColor(R.color.tct_black));
+            foodPepperyMost.setTextColor(getResources().getColor(R.color.tct_gray));
+            break;
+        case R.id.food_taste_peppery_most:
+            Log.i("hao", "HomeFragment onClick food_taste_peppery_most");
+            foodPepperyNo.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyLittle.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyMiddle.setBackground(getResources().getDrawable(R.drawable.text_field));
+            foodPepperyMost.setBackground(getResources().getDrawable(R.drawable.text_field_selected_black));
+            foodPepperyNo.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodPepperyLittle.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodPepperyMiddle.setTextColor(getResources().getColor(R.color.tct_gray));
+            foodPepperyMost.setTextColor(getResources().getColor(R.color.tct_black));
+            break;
+        case R.id.food_comment_num:
+            Log.i("hao", "HomeFragment onClick food_comment_num");
+            break;
+        default:
+            break;
         }
     }
 
